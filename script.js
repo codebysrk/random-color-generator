16ac2ec03239b02e633dddd58bdb259bfaf906a7
Shahrukh Khan <153599461+codebysrk@users.noreply.github.com>
2025-12-21 19:55:55 +0530

    fix some logic

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const box = document.querySelector(".box");
 const btn = document.querySelector("button");
+const getRandomNum = (max, min = 0) =>
+  Math.round(Math.random() * (max - min + 1) + min);
 
 btn.addEventListener("click", () => {
-  const r = Math.round(Math.random() * 255);
-  const g = Math.round(Math.random() * 255);
-  const b = Math.round(Math.random() * 255);
-  const rgb = `rgb(${r},${g},${b})`;
+  const r = getRandomNum(255);
+  const g = getRandomNum(255);
+  const b = getRandomNum(255);
+  const rgb = `rgb(${r}, ${g}, ${b})`;
   box.style.backgroundColor = rgb;
 });
